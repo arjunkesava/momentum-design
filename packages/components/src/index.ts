3b94e2528b91a198d25436403f4b9c56fc0cf0b3
shakti97 <shaktisingh230797@gmail.com>
2025-01-31 11:09:17 +0530

    chore: addressed comment & added e2e specs

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -11,6 +11,7 @@ import Marker from './components/marker';
 import Divider from './components/divider';
 import Avatarbutton from './components/avatarbutton';
 import Link from './components/link';
+import Radio from './components/radio';
 
 import type { TextType } from './components/text/text.types';
 
@@ -28,6 +29,7 @@ export {
   Divider,
   Avatarbutton,
   Link,
+  Radio,
 };
 
 export type {
