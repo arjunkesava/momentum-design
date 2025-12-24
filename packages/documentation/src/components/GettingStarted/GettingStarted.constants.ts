bebcc9cb0f7d8984ae1ed4e51e27806a6bd22fb8
Arjun Kesava <arjun.kesava@gmail.com>
2025-12-24 19:50:21 +0530

    perf: add getting started component

---
+++ packages/documentation/src/components/GettingStarted/GettingStarted.constants.ts
@@ -0,0 +1,169 @@
+export const designerCards = [
+  {
+    title: 'Design Guidelines',
+    description: 'All of Momentum Guidelines on design, typography, tokens, and more in one place.',
+    link: 'https://www.figma.com/files/project/18997192',
+  },
+  {
+    title: 'Component Usage',
+    description: 'Read about our components, how to use them in product, and some useful tips on accessibility.',
+    link: 'https://www.figma.com/files/project/35746329',
+  },
+  {
+    title: 'Design Tokens',
+    description: 'Understand our design tokens, how they’re used, and how to implement them appropriately in product.',
+    link: 'https://www.figma.com/files/project/33422626',
+  },
+  {
+    title: 'Assets Libraries',
+    description: `We have iconography, illustration, and brand visual libraries that can be used by
+    designers and engineers.`,
+    link: 'https://www.figma.com/files/project/33422718',
+  },
+  {
+    title: 'Figma Tools',
+    description: 'We also have useful tools for designers like a Workshopping Toolkit or our Presentation Library.',
+    link: 'https://www.figma.com/files/project/32740140',
+  },
+];
+
+export const trainingCards = [
+  {
+    title: 'Momentum Academy',
+    description: 'A full course on everything you need to know to use Momentum and all its libraries.',
+    link: 'https://www.figma.com/design/CggYt9i1gIxDHHwVoSQLqP/Momentum-Academy---Build-v1.8?m=auto',
+  },
+  {
+    title: 'Figma for Engineers',
+    description: 'If you’re an engineer, this course will help you understand how to use Figma.',
+    style: 'margin-bottom: 24px;',
+    link: 'https://www.figma.com/design/XFvX9lWbe42hEk3V8s4ee6/Figma-for-Devs',
+  },
+];
+
+export const coreLibraryCards = [
+  {
+    title: 'Momentum Web',
+    description: 'Momentum’s shared, consumable technology agnostic library for web products across Webex.',
+    link: 'https://www.figma.com/design/9qhjTsC97Q3W8ZsbnTiZho/%F0%9F%A7%A9-Momentum-Web---Component-Library',
+    linkTitle: 'Momentum Web Figma',
+  },
+];
+
+export const CXSolutionCards = [
+  {
+    title: 'Agent / Supervisor Desktop',
+    description: 'All of Momentum Guidelines on design, typography, tokens, and more in one place.',
+    link: 'https://www.figma.com/design/ROb0RXyi29XrpVFU8EoueC/%F0%9F%A7%A9Contact-Center---Agent%2FSupervisor-Desktop',
+    linkTitle: 'Figma Library',
+  },
+  {
+    title: 'Flow Designer',
+    description: 'Read about our components, how to use them in product, and some useful tips on accessibility.',
+    link: 'https://www.figma.com/design/WftUFQTyZUqmOvgeUZPdHf/%F0%9F%A7%A9-Contact-Center---Flow-Designer',
+    linkTitle: 'Figma Library',
+  },
+];
+
+export const platformUXCards = [{
+  title: 'Control Hub',
+  description: 'All of Momentum Guidelines on design, typography, tokens, and more in one place.',
+  link: 'https://www.figma.com/design/aGKJ0hOgs3KT0siyGSEZ2D/%F0%9F%A7%A9-Platform-UX---Web',
+  linkTitle: 'Figma Library',
+}];
+
+export const webexAppCards = [
+  {
+    title: 'Webex - Windows',
+    description: 'The Webex - Windows component library for designers in figma. *Not owned by Momentum.*',
+    link: 'https://www.figma.com/design/NaNrfXjygZtRgMfHAFHjsp/%F0%9F%A7%A9-Webex-App---Windows',
+    linkTitle: 'Figma Library',
+  },
+  {
+    title: 'Webex - MacOS',
+    description: 'The Webex - MacOS component library for designers in figma. *Not owned by Momentum.*',
+    link: 'https://www.figma.com/design/H19CutixoN8SJGO5EXwcc8/%F0%9F%A7%A9-Webex-App---MacOS',
+    linkTitle: 'Figma Library',
+  },
+  {
+    title: 'Webex - Web',
+    description: 'The Webex - Windows component library for designers in figma.',
+    style: 'margin-bottom: 24px;',
+    link: 'https://www.figma.com/design/FmXv8xvun1xAZiOMfztwqg/%F0%9F%A7%A9-Webex-App---Web',
+    linkTitle: 'Figma Library',
+  },
+  {
+    title: 'Webex - Android',
+    description: 'The Webex - Android component library for designers in figma.',
+    style: 'margin-bottom: 24px;',
+    link: 'https://www.figma.com/design/NaNrfXjygZtRgMfHAFHjsp/%F0%9F%A7%A9-Webex-App---Windows',
+    linkTitle: 'Figma Library',
+  },
+  {
+    title: 'Webex - iOS',
+    description: 'The Webex - iOS component library for designers in figma.',
+    link: 'https://www.figma.com/design/H19CutixoN8SJGO5EXwcc8/%F0%9F%A7%A9-Webex-App---MacOS?m=auto',
+    style: 'margin-bottom: 24px;',
+    linkTitle: 'Figma Library',
+  },
+];
+
+export const webexSpacesCards = [
+  {
+    title: 'Momentum Design Support',
+    description: 'A Webex space for design questions on the Momentum Design System.',
+    link: 'https://eurl.io/#l2f1fJ6Do',
+    linkTitle: 'Link to Webex',
+  },
+  {
+    title: 'Momentum Engineering Support',
+    description: 'A Webex space for engineering questions on the Momentum Design System.',
+    link: 'https://eurl.io/#BzmbetHLm',
+    linkTitle: 'Link to Webex',
+  },
+];
+
+export const officeHoursCards = [
+  {
+    title: 'Momentum Office Hours #1',
+    subTitle: 'Best for US timezone',
+    meetingLink: 'https://cisco.webex.com/cisco/j.php?MTID=m9de795bb2cc2b877333934fb1b148b6c',
+    timeZones: [
+      { region: 'US:', time: 'Tuesdays @ 1:30–2:30pm UTC -7', bold: true },
+      { region: 'Europe:', time: 'Tuesdays @ 9:30–10:30pm UTC +1', bold: false },
+      { region: 'India:', time: 'Wednesdays @ 1–2am UTC +5:30', bold: false },
+      { region: 'China:', time: 'Wednesdays @ 4:30–5:30am UTC +8', bold: false },
+    ],
+  },
+  {
+    title: 'Momentum Office Hours #2',
+    subTitle: 'Best for Europe/India timezone',
+    meetingLink: 'https://cisco.webex.com/cisco/j.php?MTID=mb3bff49c3e32a09bb8ad92048a372cf8',
+    timeZones: [
+      { region: 'US:', time: 'Tuesdays @ 8-9am UTC -7', bold: false },
+      { region: 'Europe:', time: 'Tuesdays @ 4-5pm UTC +1', bold: true },
+      { region: 'India:', time: 'Tuesdays @ 8:30-9:30pm UTC +5:30', bold: true },
+      { region: 'China:', time: 'Tuesdays @ 11pm-12am UTC +8', bold: false },
+    ],
+  },
+  {
+    title: 'Momentum Office Hours #3',
+    subTitle: 'Best for India/China timezone',
+    meetingLink: 'https://cisco.webex.com/cisco/j.php?MTID=mb3bff49c3e32a09bb8ad92048a372cf8',
+    timeZones: [
+      { region: 'US:', time: 'Thursdays @ 6-7pm UTC -7', bold: false },
+      { region: 'Europe:', time: 'Fridays @ 2-3am UTC +1', bold: false },
+      { region: 'India:', time: 'Fridays @ 6:30-7:30am UTC +5:30', bold: true },
+      { region: 'China:', time: 'Fridays @ 9-10am UTC +8', bold: true },
+    ],
+  },
+];
+
+export const requestFormCards = [
+  {
+    title: 'Request form',
+    description: 'Submit a request to the team for a design system change you think would be helpful.',
+    link: 'https://cisco.webex.com/cisco/j.php?MTID=mb3bff49c3e32a09bb8ad92048a372cf8',
+    linkTitle: 'Make a request in Asana',
+  },
+];
